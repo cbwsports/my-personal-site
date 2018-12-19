dad3cc0b433a947555c25d23b81d84bfd2715774
Chris <cbwsports@Chriss-MBP.attlocal.net>
2018-12-18 22:14:25 -0500

    commented autogenerated code from app.component. Snuck in navigation component file as well.

---
+++ src/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navi',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class AppComponent {
+  title = 'Developer';
+  name = 'Christopher Wright';
+  personal_image = 'assets/images/luffy_wanted_pic.jpg';
+}
