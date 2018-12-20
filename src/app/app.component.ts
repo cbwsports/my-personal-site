8cea3c1b3ab230c1694f432c82fb8a315ce477d7
Chris <cbwsports@Chriss-MBP.attlocal.net>
2018-12-19 22:11:07 -0500

    Created the base componenets for my site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component'
+import { ContentComponent } from './content/content.component'
+import { FooterComponent } from './footer/footer.component'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
