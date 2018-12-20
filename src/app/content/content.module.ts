2a0358616bf8f9c0258dd94008afed032af6a193
Chris <cbwsports@Chriss-MBP.attlocal.net>
2018-12-20 01:50:08 -0500

    Created content components for about, skills, experience, testimonials, and contact. I also removed unnecessary imports in app.component.ts

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ContentComponent } from './content.component';
+import { BioComponent } from './bio/bio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-            ContentComponent
+            ContentComponent,
+            BioComponent,
+            SkillsComponent,
+            ExperienceComponent,
+            TestimonialsComponent,
+            ContactComponent
   				],
   imports: [
     BrowserModule
---
+++ src/app/content/testimonials/testimonials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonial',
+  templateUrl: './testimonials.component.html',
+  styleUrls: ['./testimonials.component.css']
+})
+export class TestimonialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
